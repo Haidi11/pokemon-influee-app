09d2070afa2fad0832d33f573b94c50753f78670
Haidi11 <39265042+Haidi11@users.noreply.github.com>
2024-07-03 19:09:00 +0200

    Added flag to not show selected element on load and minor style fixes.

--- src/components/PokemonList.jsx
+++ src/components/PokemonList.jsx
@@ -47,7 +47,6 @@ const PokemonList = () => {
                 console.error('Error fetching data:', error);
             }
         };
-
         fetchPokemonList();
     }, []);
 
@@ -80,55 +79,53 @@ const PokemonList = () => {
     if (pokemonList.length === 0) return <p>Loading...</p>;
 
     return (
-        <>
-            <div className="flex">
-                <div className="w-133 min-w-133 h-164 flex flex-col justify-center items-center rounded overflow-hidden bg-white shadow-lg">
-                    <h2 className='font-montserrat text-black font-sans text-lg font-bold tracking-tight text-center mb-5'>Card Stack</h2>
-                    <div className='w-84 h-106'>
-                        <PokemonCard onSelect={handleSelectPokemon} pokemon={pokemonList[currentIndex]} />
-                    </div>
-                </div>
-
-                <div className='w-screen h-screen flex justify-center items-center mr-133'>
-                    <div className="max-w-sm rounded overflow-hidden h-360 w-283 bg-white shadow-lg">
-                        {selectedPokemon && (
-                            <div className="px-6 py-4">
-                                <img className="w-[252px] h-[256px] mx-auto" src={selectedPokemon.sprite} alt={selectedPokemon.name} />
-                                <div className="flex mx-auto justify-between items-center">
-                                    <div className="text-center flex">
-                                        <div className="font-montserrat capitalize text-black font-bold text-lg mb-2 mr-1.5">{selectedPokemon.name}</div>
-                                        <div className="flex h-[18px]">
-                                            {selectedPokemon.types.map((type, idx) => (
-                                                <div key={idx} className="capitalize rounded-lg p-1 text-black font-montserrat font-bold text-[10px] font-semibold text-center border-gray-300 shadow-md opacity-100 bg-white mr-1">
-                                                    {type}
-                                                </div>
-                                            ))}
-                                        </div>
-                                    </div>
-                                    <div className="text-customGray font-montserrat font-black text-[14px] tracking-wide text-center">
-                                        <span className="text-xs">#</span>{selectedPokemon.baseExperience}
-                                    </div>
-                                </div>
-
-                                <div className="flex">
-                                    <div className="text-customGray font-montserrat font-black text-base">{selectedPokemon.height}<span className="text-xs uppercase">dm</span></div>
-                                    <div className="text-customGray font-montserrat font-black text-base ml-[22px]">{selectedPokemon.weight / 10}<span className="text-xs uppercase">kg</span></div>
-                                </div>
-                            </div>
-                        )}
-                    </div>
-                </div>
-
-                <div className="flex absolute bottom-12 inset-x-0 flex justify-center">
-                    <div className="mr-7">
-                        <CardDeck title="Deck 1" onDropPokemon={handleDropPokemon1} initialDeck={deck1} />
-                    </div>
-                    <div className="ml-7">
-                        <CardDeck title="Deck 2" onDropPokemon={handleDropPokemon2} initialDeck={deck2} />
-                    </div>
+        <div className="flex">
+            <div className="w-133 min-w-133 h-164 flex flex-col justify-center items-center rounded overflow-hidden bg-white shadow-lg">
+                <h2 className='font-montserrat text-black font-sans text-lg font-bold tracking-tight text-center mb-5'>Card Stack</h2>
+                <div className='w-84 h-106'>
+                    <PokemonCard onSelect={handleSelectPokemon} pokemon={pokemonList[currentIndex]} />
                 </div>
             </div>
-        </>
+
+            <div className='w-screen h-screen flex justify-center items-center mr-133'>
+                {selectedPokemon && (
+                    <div className="max-w-sm rounded overflow-hidden h-360 w-283 bg-white shadow-lg">
+                        <div className="px-6 py-4">
+                            <img className="w-[252px] h-[256px] mx-auto" src={selectedPokemon.sprite} alt={selectedPokemon.name} />
+                            <div className="flex mx-auto justify-between items-center">
+                                <div className="text-center flex items-center">
+                                    <div className="font-montserrat capitalize text-black font-bold text-lg mr-1.5">{selectedPokemon.name}</div>
+                                    <div className="flex h-[18px]">
+                                        {selectedPokemon.types.map((type, idx) => (
+                                            <div key={idx} className="capitalize rounded-lg px-1 text-black font-montserrat font-bold text-[10px] font-semibold text-center border border-gray-200 shadow-md opacity-100 bg-white mr-1">
+                                                {type}
+                                            </div>
+                                        ))}
+                                    </div>
+                                </div>
+                                <div className="text-customGray font-montserrat font-black text-[14px] tracking-wide text-center">
+                                    <span className="text-[10px] uppercase">#</span>{selectedPokemon.baseExperience}
+                                </div>
+                            </div>
+
+                            <div className="flex">
+                                <div className="text-customGray font-montserrat font-black text-base mr-[22px]">{selectedPokemon.weight / 10}<span className="text-[10px] uppercase">kg</span></div>
+                                <div className="text-customGray font-montserrat font-black text-base">{selectedPokemon.height}<span className="text-[10px] uppercase">cm</span></div>
+                            </div>
+                        </div>
+                    </div>
+                )}
+            </div>
+
+            <div className="flex absolute bottom-12 inset-x-0 flex justify-center">
+                <div className="mr-7">
+                    <CardDeck title="Deck 1" onDropPokemon={handleDropPokemon1} initialDeck={deck1} />
+                </div>
+                <div className="ml-7">
+                    <CardDeck title="Deck 2" onDropPokemon={handleDropPokemon2} initialDeck={deck2} />
+                </div>
+            </div>
+        </div>
     );
 };
 
